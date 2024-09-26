42b082f4c9f580c2d64b257b1c23b933730863ce
Arturo Vargas Cuevas <avargas2705@outlook.com>
2024-09-26 13:34:15 -0600

    first draft

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+function App() {
+  return (
+    <Container
+      maxWidth="sm"
+      sx={{
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        textAlign: 'center',
+      }}
+    >
+      <Typography variant="h4" gutterBottom>
+        Arturo Vargas Cuevas
+      </Typography>
+      <Button
+        variant="contained"
+        color="primary"
+        href="./public/CV_English.pdf" // Link to your PDF file in the public folder
+        download="ArturoVargasCuevas_CV_English.pdf" // Trigger download with a specific file name
+        sx={{ mt: 2 }}
+      >
+        Descargar CV
+      </Button>
+    </Container>
+  );
+}
+
+export default App;
